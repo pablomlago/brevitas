261fd7c0f5bc444638d18c18948fe448da7f2568
Pablo Monteagudo Lago <pablo.monteagudolago@amd.com>
2025-02-28 16:00:27 +0000

    Bug fixes

--- src/brevitas_examples/llm/main.py
+++ src/brevitas_examples/llm/main.py
@@ -284,10 +284,6 @@ def quantize_llm(args, extra_args=None):
         device=None,
         fuse_sequences=args.fuse_sequences)
 
-    if args.awq_scale or args.awq_clip:
-        awq_regions = fused_awq_scaling_no_fx(
-            model, calibration_loader) if args.custom_awq_regions else None
-
     if args.optimize_rotations:
         # Extra arguments should be used as training arguments for rotation optimization
         rot_optimization_args = parse_rotation_optimization_args(extra_args=extra_args)
@@ -318,6 +314,10 @@ def quantize_llm(args, extra_args=None):
 
     if args.replace_rmsnorm:
         model = replace_rmsnorm_with_torch(model, model.config)
+
+    if args.awq_scale or args.awq_clip:
+        awq_regions = fused_awq_scaling_no_fx(
+            model, calibration_loader) if args.custom_awq_regions else None
 
     if require_fx:
         if model.__class__.__name__ in _SUPPORTED_MODELS and not args.replace_rmsnorm:
